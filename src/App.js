06ca7cd2aa4ea4bb1ff709f665bfcec4ca52e269
Adrienne Cheng <eirdac@users.noreply.github.com>
2022-04-22 23:30:41 -0400

    redoing code + css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect } from 'react';
+import Button from "@mui/material/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App() { 
+
+    const [locations, setLocations] = useState("london");
+
+    const [weather, setWeather] = useState({});
+
+    const [photos, setPhotos] = useState([]);
+
+    const OW_KEY = "e597543ad67a406d802f893c58ce667b";
+    const US_KEY = "I2XingUSOTerv-C-S2RP_qxkSl0fb7_-gDOp2NCpd_g";
+ 
+    // useEffect = when ready to change state, run this function
+    useEffect(() => {
+      submit();
+    }, []);
+    
+
+    // calling the weather API
+    function submit() {
+      // units get turned from K to degrees
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${locations}&appid=${OW_KEY}&units=metric`)
+
+      .then((res) => {
+        if (res.ok){
+          console.log(res.status);
+          return res.json();
+        } else {
+          if (res.status === 404)
+          {
+            return alert("Oops! There is an error. (wrong location)");
+          }
+          alert("Oops there seems to be an error!");
+          throw new Error("you have an error");
+        }
+      })
+      .then((object) => {
+        setWeather(object);
+        console.log(weather);
+      })
+
+      .catch((error) => console.log(error));
+
+
+      fetch(`https://api.unsplash.com/search/photos?query=${locations}&client_id=${US_KEY}`
+      )
+
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("You made a mistake!");
+        }
+      })
+
+      .then((data) => {
+        console.log(data);
+        setPhotos(data?.results[0]?.urls.raw);
+      })
+      .catch((error) => console.log(error));
+    }
+
+    return (<div className="App">
+      <div className="wrapper2"><center>WEATHER APP</center></div>
+
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" value={locations} onChange={(e) => setLocations(e.target.value)} placeholder="Enter your location" className="location_input"></input>
+          <Button onClick={submit}>Search</Button>
+        </div>
+
+          <div className="app_data">
+            <p className="temp">Current temp: {weather?.main?.temp}</p>
+          </div>
+          <img className="app_img" src={photos} alt=""></img>
+      </div>
+
     </div>
-  );
+    );
 }
-
+  
 export default App;
---
+++ src/TestComp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class TestComp extends React.Component{
+    render() {
+        return <div className="test">Linked to the App Component</div>
+    }
+}
+
+export default TestComp;
